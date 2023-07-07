36524c79a3985e73e9b4a2476c38e84f186fbe26
denis.topallaj <denis.topallaj@student.howest.be>
2023-07-07 16:35:16 +0200

    add: storage functionality that keeps the correct and incorrect ids of exercises

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,14 @@ import Home from "./Home";
 import Exercise from "./Exercise";
 
 function App() {
+	function createLocalStorageEnvironment() {
+		if (localStorage.getItem("correct") && localStorage.getItem("incorrect")) return;
+		localStorage.setItem("correct", JSON.stringify([]));
+		localStorage.setItem("incorrect", JSON.stringify([]));
+	}
+
+	createLocalStorageEnvironment();
+
 	return (
 		<div className="App">
 			<Routes>
--- client/src/Exercise.tsx
+++ client/src/Exercise.tsx
@@ -2,6 +2,10 @@ import { useNavigate, useParams } from "react-router-dom";
 import DrawCanvas from "./DrawCanvas";
 import hiraganaData from "./hiragana.json";
 
+interface IExerciseResponse {
+	prediction: boolean;
+}
+
 function Exercise() {
 	const { letter } = useParams();
 	const navigate = useNavigate();
@@ -18,6 +22,7 @@ function Exercise() {
 		const context = canvas.getContext("2d");
 		if (!context) return;
 		context.clearRect(0, 0, canvas.width, canvas.height);
+		canvas.style.backgroundColor = "white";
 	}
 
 	function sendPrediction() {
@@ -37,12 +42,54 @@ function Exercise() {
 			}),
 		})
 			.then((response) => response.json())
-			.then((data) => {
-				console.log(data);
+			.then((data: IExerciseResponse) => {
+				handlePrediction(data.prediction);
 			})
 			.catch((error) => {
 				console.error("Prediction request error:", error);
 			});
+	}
+
+	function handlePrediction(result: boolean) {
+		const canvas = document.querySelector("canvas");
+		const idOfExercise = hiraganaData[letter as keyof typeof hiraganaData];
+		if (!canvas) return;
+		const color = result ? "green" : "red";
+		canvas.style.backgroundColor = color;
+
+		// add to storage
+		addToStorage(result, idOfExercise);
+
+		// remove from other storage if it exists
+		removeFromOtherStorage(result, idOfExercise);
+	}
+
+	function addToStorage(result: boolean, idOfExercise: number) {
+		const storageKey = result ? "correct" : "incorrect";
+		const storage = localStorage.getItem(storageKey);
+		if (!storage) return;
+		const parsedStorage: number[] = JSON.parse(storage) as number[];
+		
+		// check if the exercise has already been added to the storage
+		if (parsedStorage.includes(idOfExercise)) return;
+
+		const newStorage = [...parsedStorage, idOfExercise];
+		localStorage.setItem(storageKey, JSON.stringify(newStorage));
+
+	}
+
+	function removeFromOtherStorage(result: boolean, idOfExercise: number) {
+		const otherStorageKey = result ? "incorrect" : "correct";
+		const otherStorage = localStorage.getItem(otherStorageKey);
+
+		if (!otherStorage) return;
+		const parsedOtherStorage: number[] = JSON.parse(otherStorage) as number[];
+
+		const index = parsedOtherStorage.indexOf(idOfExercise);
+		if (index > -1) {
+			parsedOtherStorage.splice(index, 1);
+			localStorage.setItem(otherStorageKey, JSON.stringify(parsedOtherStorage));
+		}
 	}
 
 	return (
